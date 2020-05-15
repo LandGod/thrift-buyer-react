eef6ea7304f85390eb3bc10a128857e69cc6c0c4
Dan Gold <tentativechaos@gmail.com>
2020-05-15 14:24:06 -0700

    Added effect for reporting rating change to parent with debounce.

--- client/src/components/RatingBox/index.tsx
+++ client/src/components/RatingBox/index.tsx
@@ -1,14 +1,46 @@
-import React, { useMemo } from "react";
+import React, { useEffect } from "react";
 import "./styles.css";
+import { useState } from "@storybook/addons";
 
-interface RatingBoxProps {
-    type?: 'stars' | 'price'; // Default 'stars'
-    intialScore?: number;  // Default undefined
-    userCanEdit?: boolean;  // Default undefined (false)
+export interface RatingBoxProps {
+  type?: "stars" | "price"; // Default 'stars'
+  initialRating?: number; // Default undefined
+  userCanEdit?: boolean; // Default undefined (false)
+  ratingReportsTo: (newUserRating: number) => void; // Requires function that reports any change in rating to the parent component.
 }
 
+export default function RatingBox({
+  type = "stars",
+  userCanEdit,
+  initialRating,
+  ratingReportsTo,
+}: RatingBoxProps) {
+  const [currentRating, updateCurrentRating]: [
+    number,
+    (number: number) => void
+  ] = useState(initialRating || 0);
 
+  // Report changes in the rating to parent element, but not more than once per second
+  useEffect(() => {
+    const reportDebounceTimer = setTimeout(() => {
+      ratingReportsTo(currentRating);
+    }, 1000);
+    return () => {
+      clearTimeout(reportDebounceTimer);
+    };
+  }, [currentRating]);
 
-export default function RatingBox ( { type='stars' }:RatingBoxProps ) {
-    
+  if (type === "stars") {
+    return (
+      <div>
+        <div
+          className={
+            !userCanEdit ? "rating-box rating-box--view-only" : "rating-box"
+          }
+        >
+          <span className={}></span>
+        </div>
+      </div>
+    );
+  }
 }
