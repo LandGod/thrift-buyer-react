2a4a62c76f6190247a4fed75157bd113e4f27a46
Dan Gold <tentativechaos@gmail.com>
2020-05-30 16:32:27 -0700

    Added toggle track style to toggle component.

--- client/src/components/ToggleSwitch/index.tsx
+++ client/src/components/ToggleSwitch/index.tsx
@@ -19,6 +19,7 @@ export default function ToggleSwitch({
 
   return (
     <div className="switch__outline" onClick={toggleFunction} >
+      <div className="switch__inner-track"></div>
       <div className="switch__circle" style={{transform: `${ position === 'left' ? "translate(0)" : "translate(33px)"}`}}></div>
     </div>
   );
